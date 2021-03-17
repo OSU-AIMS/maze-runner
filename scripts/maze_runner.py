7448221caaba256d4a9b33340bc98dc21ab5be4b
acbuynak <acbuynak@gmail.com>
2021-03-16 23:47:43 -0400

    Dev - Motion Testing SavePoint, Misc
    Misc updates.

--- scripts/maze_runner.py
+++ scripts/maze_runner.py
@@ -8,7 +8,7 @@
 ##                                                 ##
 #####################################################
 
-# Software License Agreement (BSD License)
+# Software License Agreement (Apache 2.0 License)
 #
 # Copyright (c) 2021, The Ohio State University
 # Center for Design and Manufacturing Excellence (CDME)
@@ -19,7 +19,7 @@
 
 #####################################################
 
-
+import os
 import numpy as np
 
 from process_path import prepare_path_transforms_list
@@ -27,17 +27,153 @@ from process_path import prepare_path_tf_ready
 
 from transformations import transformations
 
-import robot_support
+#import robot_support
+from robot_support import all_close
+from robot_support import moveManipulator
 
 
-def retrieve_pose_from_dream3d():
+## From Robot Support
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+from math import pi, radians
+from std_msgs.msg import String
+from moveit_commander.conversions import pose_to_list
+from motoman_msgs.srv import ReadSingleIO, WriteSingleIO
+
+## Quaternion Tools
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+
+## Maze Runner Specific
+import csv
+import json
+from rospy_message_converter import json_message_converter
+from d3d_post_process import Dream3DPostProcess
+
+#####################################################
+## Import Realsense Control Tools
+from realsense_vision_tools import REALSENSE_VISION, timer_wait
+
+# Used for Advanced Mode Setup & Point Cloud Streaming
+import pyrealsense2 as rs
+import time
+#import sys
+
+# Used for Depth Filtering
+#import numpy as np                        # fundamental package for scientific computing
+import matplotlib.pyplot as plt           # 2D plotting library producing publication quality figures
+
+# Color Image processing
+import cv2
+
+#####################################################
+
+
+def retrieve_pose_from_dream3d(workspace_path, image_path):
   """
   Input Dream3D. Don't know what this looks like. 
   Place holder! Yay!
   """
+  import os
+  import subprocess
+  import json
+
+  pipeline = os.path.dirname(workspace_path) +'/dream3d_pipelines/filter_irs_image.json'
+
+  path_redDot   = workspace_path + '/feature_redDot.csv'
+  path_greenDot = workspace_path + '/feature_greenDot.csv'
+  path_blueDot  = workspace_path + '/feature_blueDot.csv'
+
+  # Setup & Run Dream3D / Simple Pipeline
+  with open(pipeline, 'r') as jsonFile:
+    data = json.load(jsonFile)
+
+  data["00"]["FileName"] = image_path
+
+  data["23"]["FeatureDataFile"] = path_redDot
+  data["24"]["FeatureDataFile"] = path_greenDot
+  data["25"]["FeatureDataFile"] = path_blueDot
+
+  data["26"]["OutputFilePath"] = workspace_path + '/feature_redDot_array.csv'
+  data["28"]["OutputFilePath"] = workspace_path + '/filter_irs_image.dream3d'
+
+  data["31"]["FileName"] = workspace_path + '/mask_MazeOnly.tiff'
+  data["34"]["FileName"] = workspace_path + '/mask_mostlyBlue.tiff'
+
+  with open(pipeline, 'w') as jsonFile:
+    json.dump(data, jsonFile)
+
+  subprocess.call(["/opt/dream3d/bin/PipelineRunner", "-p", pipeline])
+
+  # Post Process Features to find Rotation
+  color_names = ["red", "green", "blue"]
+  centroid_filepaths = [path_redDot, path_greenDot, path_blueDot]
+
+  d3d = Dream3DPostProcess(color_names, centroid_filepaths, tolerance=80)
+  rotationMatrix = d3d.rotMatrix
+  centroid = d3d.mazeCentroid
+
+  return centroid, rotationMatrix
+
+
+class DataSaver(object):
+  """
+  All data saving functions wrapped in one tool.
+  """
+  def __init__(self, mzrun_ws, robot, camera):
+    #Setup
+    self.workspace = mzrun_ws
+    self.robot = robot
+    self.camera = camera
+
+    setup_dict = {}
+    setup_dict['counter'] = []
+    setup_dict['poses'] = []
+    setup_dict['images'] = []
+    setup_dict['maze_rotationMatrix'] = []
+    setup_dict['maze_centroid'] = []
+
+    self.all_data = setup_dict
+
+  def capture(self, img_counter, find_maze=False):
+    img_path = self.camera.capture_singleFrame_color(self.workspace + '/' + str(img_counter))
+    pose = self.robot.lookup_pose()
+
+    if find_maze: 
+      # Run Vision Pipeline, find Location & Rotation of Maze
+      centroid, rotationMatrix = retrieve_pose_from_dream3d(self.workspace, img_path)
+      self.save_data(self.workspace, img_counter, img_path, pose, centroid, rotationMatrix)
+    else:
+      self.save_data(self.workspace, img_counter, img_path, pose, np.array([0]), np.array([0]))
+
+  def save_data(self, mzrun_ws, img_counter, img_path, pose, maze_centroid, maze_rotationMatrix) :
+    """
+    Take Photo AND Record Current Robot Position
+    :param robot:     Robot Instance
+    :param camera:    Realsense Camera Instance
+    :param mzrun_ws:  Local Workspace Path for Variables
+    :param image_counter: Counter Input for consequitive location is saved at. 
+    :return           Updated Storage Dicitonary
+    """
+    add_data = self.all_data
+
+    add_data['counter'].append(img_counter)
+    add_data['images'].append(img_path)
+    add_data['poses'].append(json_message_converter.convert_ros_message_to_json(pose))
+    add_data['maze_centroid'].append(np.ndarray.tolist(maze_centroid))
+    add_data['maze_rotationMatrix'].append(np.ndarray.tolist(maze_rotationMatrix))
+
+    with open(mzrun_ws + '/camera_poses.json', 'w') as outfile:
+      json.dump(add_data, outfile, indent=4)
+
+    self.all_data = add_data
 
 
 
+#####################################################
 def main():
 
   print("")
@@ -51,51 +187,112 @@ def main():
 
 
 
-  # Set Maze to be Solved
-  #solved_maze_path = raw_input('Input PreSolved Maze Path: ')
-  print("Input Solve Maze Path:  'tiny_path_soln.csv'")
-  solved_maze_path = 'tiny_path_soln.csv'
+  if False:
+    # Set Maze to be Solved
+    #solved_maze_path = raw_input('Input PreSolved Maze Path: ')
+    print("Input Solve Maze Path:  'tiny_path_soln.csv'")
+    solved_maze_path = 'tiny_path_soln.csv'
 
-  # XY Process Path into Transformation List
-  path_as_xyz = prepare_path_transforms_list(solved_maze_path)
-  path_as_tf_matrices = prepare_path_tf_ready(path_as_xyz)
+    # XY Process Path into Transformation List
+    path_as_xyz = prepare_path_transforms_list(solved_maze_path)
+    path_as_tf_matrices = prepare_path_tf_ready(path_as_xyz)
 
-  # Find & Apply Rigid Body Transform to Maze 
-  tf = transformations()
-  body_rot = np.matrix('0 -1 0; 1 0 0; 0 0 1')   # rot(z,90deg)    ##TODO: get from DREAM3D pipeline  body_rot=retrieve_pose_from_dream3d(_)
-  body_transl = np.matrix('0; 0; 0')
-  body_frame = tf.generateTransMatrix(body_rot, body_transl)
-  print('Maze Origin Frame Calculated to be @ ', body_frame)
+    # Find & Apply Rigid Body Transform to Maze 
+    tf = transformations()
+    body_rot = np.matrix('0 -1 0; 1 0 0; 0 0 1')   # rot(z,90deg)    ##TODO: get from DREAM3D pipeline  body_rot=retrieve_pose_from_dream3d(_)
+    body_transl = np.matrix('0; 0; 0')
+    body_frame = tf.generateTransMatrix(body_rot, body_transl)
+    print('Maze Origin Frame Calculated to be @ ', body_frame)
 
-  # Rotate Path (as Rigid Body) according to body_frame
-  path_via_fixed_frame = tf.convertPath2FixedFrame(path_as_tf_matrices, body_frame)
+    # Rotate Path (as Rigid Body) according to body_frame
+    path_via_fixed_frame = tf.convertPath2FixedFrame(path_as_tf_matrices, body_frame)
 
-  # Convert Path of Transforms to Robot Poses
-  new_path_poses = tf.convertPath2RobotPose(path_via_fixed_frame)
+    # Convert Path of Transforms to Robot Poses
+    new_path_poses = tf.convertPath2RobotPose(path_via_fixed_frame)
 
 
-############################3
-
+  ############################
+  #Debug Settings
+  motion_testing = True
+  
   try:
+
+    # Setup Robot
     robot = moveManipulator()
-    robot.set_accel(0.2)
-    robot.set_vel(0.2)
+    robot.set_accel(0.15)
+    robot.set_vel(0.15)
+
+    # Setup Camera
+    camera = REALSENSE_VISION() # Higher Resolution made difficult... (set_color=[1280,720,6], set_depth=[1280,720,6])
+    
+    # Setup Working Directory
+    dir_mzrun = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    mzrun_ws = dir_mzrun + '/mzrun_ws'
+
+    try:
+      os.makedirs(mzrun_ws, True)
+    except OSError:
+      if not os.path.isdir(mzrun_ws):
+        raise
+    os.chmod(mzrun_ws, 0777)
+
+    # Setup Image Record JSON
+    database = DataSaver(mzrun_ws, robot, camera)
+    img = 0
+
+
+    ############################
+    # Move to Known Start Position: All-Zeros
+    if motion_testing:
+      raw_input('Go to Maze Overlook Crouch Position <enter>')
+      robot.goto_named_target("maze_overlook_crouch")
+
+    # Move to Start Position
+    start_pose = robot.lookup_pose()
+    start_pose.position.z -= 0.7
+    if motion_testing:
+      robot.goto_Quant_Orient(start_pose)
+
+    # Record First Camera Set
+    timer_wait() #ensure camera has had time to buffer initial frames
+    database.capture(img, find_maze=False)
+
+
+    # Increment Robot down for testing
+    for img in range(1,4):
+
+      # Move Robot
+      raw_input('incr <enter>')
+      tmp_pose = robot.lookup_pose()
+      tmp_pose.position.z -= 0.2
+
+      if motion_testing:
+        robot.goto_Quant_Orient(tmp_pose)
+
+      database.capture(img, find_maze=True)   
+
+
+    ############################
+    # Close Vision Pipeline
+    camera.stopRSpipeline()
 
     # Move to Known Start Position: All-Zeros
-    raw_input('Go to All-Zeros Position <enter>')
-    robot.goto_all_zeros()
+    if motion_testing:
+      raw_input('Go to Maze Overlook Crouch Position <enter>')
+      robot.goto_named_target("maze_overlook_crouch")
 
-    for msg in new_path_poses:
-      print(msg)
-
-      robot.goto_Quant_Orient(msg)
-      time.sleep(0.5)
 
   except rospy.ROSInterruptException:
-      return
+    # Close Vision Pipeline
+    camera.stopRSpipeline()
+    return
 
   except KeyboardInterrupt:
-      return
+    # Close Vision Pipeline
+    camera.stopRSpipeline()
+    return
 
+
+#####################################################
 if __name__ == '__main__':
   main()
