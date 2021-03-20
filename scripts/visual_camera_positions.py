5553fa8a25c48b09bfc1113aa0ed722d53ba8653
acbuynak <acbuynak@gmail.com>
2021-03-20 15:10:32 -0400

    Dev - Camera Pose Visualizer for RViz

--- scripts/visual_camera_positions.py
+++ scripts/visual_camera_positions.py
@@ -16,27 +16,44 @@ import geometry_msgs.msg
 
 
 ## NODES ##
-def node_cameraPoseArray(inputArray):
+class node_cameraPoseArray(object):
     """ Publish and Latch a Pose Array to a rostopic. """
-    
-    # Imports
-    import rospy
-    from geometry_msgs.msg import PoseArray
 
-    # Config node
-    pub = rospy.Publisher('cameraPoseArray', PoseArray, queue_size=10) #TODO: couldn't get latch=True to work. Looping instead
-    rospy.init_node('cameraPoseArray', anonymous=False)
-    rate = rospy.Rate(1) # 10hz
+    def __init__(self, inputArray):
+        
+        # Imports
+        import rospy
+        from geometry_msgs.msg import PoseArray
 
-    message = geometry_msgs.msg.PoseArray()
-    message.header.frame_id = 'base_link'
-    message.poses = inputArray
-    
-    # Publish node
-    while not rospy.is_shutdown():
-        #rospy.loginfo(message)
-        pub.publish(message)
-        rate.sleep()
+        # Config node
+        self.pub = rospy.Publisher('camera_pose_array', PoseArray, queue_size=10, latch=True) #TODO: couldn't get latch=True to work. Looping instead
+        rospy.init_node('camera_pose_array', anonymous=False)
+        self.rate = rospy.Rate(1) # number of times per second
+
+        self.message = geometry_msgs.msg.PoseArray()
+        self.message.header.frame_id = 'origin'
+        self.message.poses = inputArray
+
+
+def update_pose_array(json_file):
+    """
+    Opens and reads in a list of poses from json
+    :param json_file: Input absolute filepath
+    :return                 List of poses in ROS message Format
+    """
+
+    import json
+    from rospy_message_converter import json_message_converter
+    import geometry_msgs.msg
+
+    with open(json_file) as file:
+        data = json.load(file)
+
+    poselist_ros_format = []
+    for i in data['poses']:
+        poselist_ros_format.append(json_message_converter.convert_json_to_ros_message('geometry_msgs/Pose', i))
+
+    return poselist_ros_format
 
 
 
@@ -44,25 +61,54 @@ def node_cameraPoseArray(inputArray):
 ## MAIN CODE ##
 def main():
 
+    from os import path
+    from time import sleep
+
     import rospy
     import numpy as np
-    import geometry_msgs.msg
-    from rospy_message_converter import json_message_converter
 
 
     # Find mzrun_ws
-    dir_mzrun = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    mzrun_ws = dir_mzrun + '/mzrun_ws'
+    #dir_mzrun = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    json_file_name = '/camera_poses.json'
+    mzrun_ws = '/home/aims-zaphod/AA_DEVL/ws_mazerunner_multibot/src/maze-runner/mzrun_ws'
+    input_json_file = mzrun_ws + json_file_name
 
-    with open(mzrun_ws + '/camera_poses.json') as file:
-         = json_message_converter.convert_json_to_ros_message(file)
 
+    # Check for File
+    while True:
+        #todo: consider switching this to 'threading.timer' to reduce processor load
+        if path.exists(input_json_file):
+            print("<< Found input camera positions file.")
+            break
+        sleep(5.0)
+
+
+    # Pre-Loop Setup
+    poselist_ros_format = update_pose_array(input_json_file)
+    # node = node_cameraPoseArray(poselist_ros_format)
+    # node.pub.publish(node.message)
 
     # Try launching ros node
     try:
-        node_cameraPoseArray(pose_geom)
+        node = node_cameraPoseArray(poselist_ros_format)
+        node.rate = rospy.Rate(1) # number of times per second
+
+
+        # Read File in loop until terminated.
+        while not rospy.is_shutdown():
+            poselist_ros_format = update_pose_array(input_json_file)
+            node.message.poses = poselist_ros_format
+
+            node.rate.sleep()
+
+            rospy.loginfo(node.message)
+            node.pub.publish(node.message)
+            print("<<< Updated Camera Pose Array Node")
+
     except rospy.ROSInterruptException:
         pass
+
 
 if __name__ == '__main__':
     main()
